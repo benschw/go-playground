adf6dbb694ad6aa73944f26ba968962b28f6d205
Ben Schwartz <benschw@gmail.com>
2015-04-20 16:29:44 -0500

    adding readme

--- duck/service/service_test.go
+++ duck/service/service_test.go
@@ -16,6 +16,8 @@ func (lb *StaticAddressGetter) GetAddress(address string) (dns.Address, error) {
 	return lb.Val, nil
 }
 
+// Component test for greeting endpoint
+// excercise running server with the client
 func TestGreetingEndpoint(t *testing.T) {
 	// given
 	expectedGreeting := "\"hello world\""
